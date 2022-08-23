5dc054c1ca63ed65903e8878e42cc69d2a67ef56
Carolina Morais Nigri <102864941+Carolina-Nigri@users.noreply.github.com>
2022-08-23 11:40:49 -0300

    Update TP01q12.c

--- TPs/01/q12/TP01q12.c
+++ TPs/01/q12/TP01q12.c
@@ -1,5 +1,9 @@
-/* Carolina Morais Nigri
+/** 
  * TP01q12 - Palindromo em C RECURSIVO
+ * 
+ * @author Carolina Morais Nigri
+ * @since 16/08/22
+ * @version 23/08/22
  */
 
 // bibliotecas
@@ -13,32 +17,47 @@ bool isPalindromo(char frase[]);
 bool isPalindromoRec(char frase[], int i, int j);
 bool isFIM(char frase[]);
 
+/** 
+ * Funcao principal: le frases ate encontrar FIM (@see isFIM) e verifica se 
+ * eh palindromo (@see isPalindromo), mostrando resultados na tela
+ * @return 0 int
+ */
 int main(){
     char frase[500];
-
-    scanf(" %500[^\n]", frase); // le frase
+    scanf(" %500[^\n]", frase);
     
-    while(!isFIM(frase)){ // sai quando frase = "FIM"
-        // se eh palindromo, imprime "SIM", se nao, "NAO"
+    while(!isFIM(frase)){
         if(isPalindromo(frase))
             puts("SIM");
         else 
             puts("NAO");
         
-        scanf(" %500[^\n]", frase); // le frase
+        scanf(" %500[^\n]", frase);
     }
 
     return 0;
 } // fim main()
 
+/** 
+ * Metodo isPalindromo: retorna funcao recursiva (@see isPalindromoRec)
+ * @param frase String
+ * @return boolean isPalindromo()
+ */
 bool isPalindromo(char frase[]){
     return isPalindromoRec(frase,0,(strlen(frase)-1));
 } // fim isPalindromo() 
 
+/** 
+ * Metodo isPalindromoRec: verifica se frase eh palindromo, 
+ * comparando os caracteres iniciais com os finais recursivamente
+ * @param frase String
+ * @param i int
+ * @param j int
+ * @return boolean palind
+ */
 bool isPalindromoRec(char frase[], int i, int j){
     bool palind = true;
 
-    // compara os primeiros char c/os ultimos, ate q o i encontre o j
     if(i < j){ 
         if(frase[i] == frase[j]){
             palind = isPalindromoRec(frase, i+1, j-1);
@@ -50,6 +69,12 @@ bool isPalindromoRec(char frase[], int i, int j){
     return palind;
 } // fim isPalindromoRec()
 
+/** 
+ * Funcao isFIM: verifica se frase eh "FIM", comparando tamanho (3) e 
+ * caractere por caractere ('F','I','M')
+ * @param frase char[]
+ * @return bool fim
+ */
 bool isFIM(char frase[]){
     bool fim = false;
 
